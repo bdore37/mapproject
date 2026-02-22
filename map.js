baccdbcbe0b6d7e015fccc1e367e661ba32557cc
Bryan <bdore@yahoo.com>
2026-02-22 11:33:49 -0600

    Update map.js
    Pulled seed from page input
    Began to add to the flood function

--- map.js
+++ map.js
@@ -1,14 +1,15 @@
 // -- Initialize global variables --
 
-let theSeed = 10071974; // 0 < X < m
-const mapWidth = 40;
-const mapHeight = 20;
+const seedInput = getDocumentById("seed");
+let theSeed = seedInput.value; // 0 < X < m
+const mapWidth = 40; // Maybe irrelevant with mapType sets
+const mapHeight = 20; // Maybe this becomes "size"
 const fullMap = [[]]; // Multidimensional ([r][c]) array
 const mapTypes = new Map();
 
 // -- Set Up Maps Structures --
 
-mapTypes.set("full", [3, 9, 15, 21, 26, 30, 34, 37, 39, 40, 40, 39, 37, 34, 30, 26, 21, 15, 9, 3]);
+mapTypes.set("full", [3, 9, 15, 21, 26, 30, 34, 37, 39, 40, 40, 39, 37, 34, 30, 26, 21, 15, 9, 3]); // Figure out how to do this programmatically
 
 // -- Objects --
 
@@ -23,7 +24,10 @@ this.pressure = 0; // Vector indicating pressure
 
 // Need to figure out how to put "neighbors" and percentage of them
 
-this.flood = function() {}; // What happens to the water layer
+this.flood = function(depth) {
+   // What happens to the water layer
+  
+};
 this.erode = function()  {}; // This might actually be used for all layers to simulate movement from "high" to low
 this.warp = function() {}; // Maybe think of another name. Simulating continental drift
 
@@ -94,5 +98,6 @@ theSeed = result;
 result = result / modulus;
 
 return result;
+
 
 }
